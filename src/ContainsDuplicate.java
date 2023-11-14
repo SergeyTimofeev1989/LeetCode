d097c0562fac9308c787c9cba60e0e04b34d22c6
Sergey <SergeyTimofeev1989@ya.ru>
2023-11-14 17:31:21 +0300

    Contains duplicate

---
+++ src/ContainsDuplicate.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        int[] array = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+        System.out.println(containsDuplicate(array));
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        boolean isContains = false;
+        Set<Integer> set = new HashSet<>();
+        for (Integer num : nums) {
+            if (set.contains(num)) {
+                isContains = true;
+                break;
+            } else {
+                set.add(num);
+                isContains = false;
+            }
+        }
+        return isContains;
+    }
+}
